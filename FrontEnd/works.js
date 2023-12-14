61b6f07ced6375741dfb56b06b920d25f435e77e
Enzo Gomes <enzo-gomes@hotmail.fr>
2023-12-14 13:30:52 +0100

    Step 1.2

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -3,16 +3,65 @@ const travaux = await reponse.json();
 
 console.log(travaux);
 
-document.querySelector(".gallery").innerHTML = "";
-
-for(let j = 0; j < travaux.length; j++){
-    const emplacementTravaux = document.createElement("figure");
-    const image = document.createElement("img");
-    const caption = document.createElement("figcaption");
-    image.src = travaux[j].imageUrl;
-    caption.innerText = travaux[j].title;
-    emplacementTravaux.appendChild(image);
-    emplacementTravaux.appendChild(caption);
-    document.querySelector(".gallery").appendChild(emplacementTravaux);
-
+function clearList() {
+    document.querySelector(".gallery").innerHTML = "";
 }
+
+clearList();
+
+function genererTravaux(travaux){
+    for(let j = 0; j < travaux.length; j++){
+        const emplacementTravaux = document.createElement("figure");
+        const image = document.createElement("img");
+        const caption = document.createElement("figcaption");
+        image.src = travaux[j].imageUrl;
+        caption.innerText = travaux[j].title;
+        emplacementTravaux.appendChild(image);
+        emplacementTravaux.appendChild(caption);
+        document.querySelector(".gallery").appendChild(emplacementTravaux);
+    
+    }
+}
+
+genererTravaux(travaux);
+
+/* 
+ * 
+ */
+
+const boutonFiltrerTous = document.querySelector(".filtrer-tous");
+
+boutonFiltrerTous.addEventListener("click", () => {
+    clearList();
+    genererTravaux(travaux);
+});
+
+const boutonFiltrerObjets = document.querySelector(".filtrer-objets");
+
+boutonFiltrerObjets.addEventListener("click", () => {
+    const projetsFiltres = travaux.filter((work) => {
+        return work.category.name === "Objets";
+    });
+    clearList();
+    genererTravaux(projetsFiltres);
+});
+
+const boutonFiltrerApparts = document.querySelector(".filtrer-apparts");
+
+boutonFiltrerApparts.addEventListener("click", () => {
+    const projetsFiltres = travaux.filter((work) => {
+        return work.category.name === "Appartements";
+    });
+    clearList();
+    genererTravaux(projetsFiltres);
+});
+
+const boutonFiltrerHotels = document.querySelector(".filtrer-hotels");
+
+boutonFiltrerHotels.addEventListener("click", () => {
+    const projetsFiltres = travaux.filter((work) => {
+        return work.category.name === "Hotels & restaurants";
+    });
+    clearList();
+    genererTravaux(projetsFiltres);
+});
