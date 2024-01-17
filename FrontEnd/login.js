cfd1712092a47834d764baafbac3e69ea1421670
Enzo Gomes <enzo-gomes@hotmail.fr>
2024-01-17 14:58:20 +0100

    final adjustments

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,9 +1,9 @@
-function verifyLogin(mail){
+/* function verifyLogin(mail){
     let emailRegExp = new RegExp("[a-z0-9.-_]+@[a-z0-9.-_]+\\.[a-z0-9.-_]+");
     if(!emailRegExp.test(mail)){
         throw new Error("Le mail est invalide")
     }
-}
+} */
 
 export function matchLogin(){
     const formulaireLogin = document.querySelector("#login form");
@@ -16,25 +16,28 @@ export function matchLogin(){
             password: pwd
         };
         const chargeUtile = JSON.stringify(requeteLogin);
-        try{
-            fetch('http://localhost:5678/api/users/login', {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: chargeUtile
-            }).then(reponseLogin => {
-                if(!reponseLogin.ok){
-                    throw new Error("Echec lors de la connexion");
-                }
-                return reponseLogin.json();
-            }).then(reponseLogin => {
-                window.localStorage.setItem("id", reponseLogin.userId);
-                window.localStorage.setItem("token", reponseLogin.token);
-                window.location.href = 'http://127.0.0.1:5500/FrontEnd/index.html';
-        });
         
-        } catch (error) {
-            console.log(error);
-        }
+        fetch('http://localhost:5678/api/users/login', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: chargeUtile
+        }).then(reponseLogin => {
+            if(!reponseLogin.ok){
+                throw new Error("Echec lors de la connexion : Email ou mot de passe incorrect");
+            }
+            return reponseLogin.json();
+        }).then(reponseLogin => {
+            window.localStorage.setItem("id", reponseLogin.userId);
+            window.localStorage.setItem("token", reponseLogin.token);
+            window.location.href = 'http://127.0.0.1:5500/FrontEnd/index.html';
+        }).catch(error => {
+            const messageLogin = document.createElement("p");
+            messageLogin.innerHTML = error;
+            document.querySelector("#login form").appendChild(messageLogin);
+        })
+        
+        
+        
         
 
     });
